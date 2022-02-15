60c5df739d098f41054626d1f4da324520f1d056
ashish.chauhan <ashish.chauhan@nutanix.com>
2022-02-15 11:25:56 +0530

    print help

--- src/main/java/com/ashish/Main.java
+++ src/main/java/com/ashish/Main.java
@@ -14,28 +14,28 @@ public class Main {
 
     public static void main(String [] args ){
         final Options options = new Options();
-        options.addOption(new Option("f", "cookie_file", true, "Input file to read cookies"));
-        options.addOption(new Option("d", "date", true, "Date to query for"));
+        Option filePathOption = new Option("f", "cookie_file", true, "Input file to read cookies");
+        Option dateOption = new Option("d", "date", true, "Date to query for");
+        filePathOption.setRequired(true);
+        dateOption.setRequired(true);
+        options.addOption(filePathOption);
+        options.addOption(dateOption);
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
         String filePath = "";
         LocalDate dateTime = null;
+        HelpFormatter formatter = new HelpFormatter();
         try {
              cmd = parser.parse(options, args);
              filePath = cmd.getOptionValue("f");
+             File file = new File(filePath);
+             dateTime = LocalDate.parse(cmd.getOptionValue("d"));
 
-            File file = new File(filePath);
-            dateTime = LocalDate.parse(cmd.getOptionValue("d"));
-            Result result  = new LogManager().processRequest(file, dateTime.atStartOfDay());
-            System.out.println(result.getData().toString());
-            /*
-                Future
-                1. Make subsequent querying efficient by preprocessing data.
-             */
-
+             Result result  = new LogManager().processRequest(file, dateTime.atStartOfDay());
+             System.out.println(result.getData().toString());
         } catch (ParseException e){
-            e.printStackTrace();
+            formatter.printHelp("java -cp quant.jar com.ashish.Main", options);
             System.out.println("Please enter valid args");
         }
         catch (FileNotFoundException e){
